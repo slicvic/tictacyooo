6ef682395c93012b047d7c5b66495f0a2c7ee06b
victor <dot@mymngmt.com>
2017-04-12 17:33:26 -0400

    Configure express to serve public static files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express    = require('express')
+const express    = require('express');
+const path       = require('path');
 const app        = express();
 const httpServer = require('http').Server(app);
 const port       = 3001;
 
 httpServer.listen(port);
+app.use(express.static(path.join(__dirname, 'public')));
