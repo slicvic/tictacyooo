77469352d2f1a3d47faf5f775834d911ab91f12f
victor <dot@mymngmt.com>
2017-04-13 21:35:02 -0400

    Abstracted games and players state into state manager service

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
+const port       = 3001;
 const express    = require('express');
 const path       = require('path');
 const app        = express();
 const httpServer = require('http').Server(app);
 const io         = require('socket.io')(httpServer);
-const port       = 3001;
-const math       = require('./helpers/math');
 const Player     = require('./models/player');
 const Game       = require('./models/game');
-const players    = {};
-const games      = {};
 const socketManager = require('./services/socket-manager')(io);
+const stateManager  = require('./services/state-manager')();
 
 httpServer.listen(port);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 socketManager.onConnect(function(socket) {
-
     this.onPlayerEnter((data) => {
         if (typeof data === 'object' && typeof data.name === 'string') {
             try {
                 const player = Player.create(data.name, socket);
-                players[player.id] = player;
+                stateManager.players[player.id] = player;
                 this.emitPlayerEnter(true, player.id);
             } catch (e) {
-                this.emitPlayerEnter(false, null, 'Whoops! We ran into a problem, no biggie, just try again!');
+                this.emitPlayerEnter(false, null, e+'Whoops! We ran into a problem, no biggie, just try again!');
             }
         } else {
             this.emitPlayerEnter(false, null, `Whoops! We didn't get your name, make sure it's at least ${Player.MINIMUM_NAME_LENGTH} characters!`);
@@ -31,61 +29,13 @@ socketManager.onConnect(function(socket) {
     });
 
     this.onPlayerAwaitingOpponent((data) => {
-        if (typeof data === 'object' && typeof data.playerId === 'string' && players[data.playerId] instanceof Player) {
-            players[data.playerId].status = Player.Status.AWAITING_OPPONENT;
+        if (typeof data === 'object' && typeof data.playerId === 'string' && stateManager.players[data.playerId] instanceof Player) {
+            const player = stateManager.players[data.playerId];
+            player.status = Player.Status.AWAITING_OPPONENT;
             this.emitPlayerAwaitingOpponent(true);
-            findOpponent(players[data.playerId]);
+            stateManager.findOpponent(player);
         } else {
             this.emitPlayerAwaitingOpponent(false, 'Whoops! We ran into a problem, no biggie, just try again!');
         }
     });
 });
-
-function findOpponent(playerX) {
-	if (!(playerX instanceof Player)) {
-		throw 'Invalid argument playerX';
-	}
-
-	for (let playerId in players) {
-		if (playerId === playerX.id || players[playerId].status !== Player.Status.AWAITING_OPPONENT) {
-			continue;
-		}
-
-		const playerO = players[playerId];
-		const firstTurn = (math.random() % 2 === 0) ? Game.Symbol.X : Game.Symbol.O;
-		const game = Game.create(playerX, playerO);
-
-		games[game.id] = game;
-
-		playerX.setStatus(Player.Status.PLAYING);
-		playerO.setStatus(Player.Status.PLAYING);
-
-		playerX.socket.emit('game.start', {
-			gameId: game.id,
-			turn: firstTurn,
-			me: {
-				symbol: Game.Symbol.X
-			},
-			opponent: {
-				symbol: Game.Symbol.O,
-				id: game.playerO.id,
-				name: game.playerO.name
-			}
-		});
-
-		playerO.socket.emit('game.start', {
-			gameId: game.id,
-			turn: firstTurn,
-			me: {
-				symbol: Game.Symbol.O
-			},
-			opponent: {
-				symbol: Game.Symbol.X,
-				id: playerX.id,
-				name: playerX.name
-			}
-		});
-
-		break;
-	}
-}
---
+++ services/state-manager.js
@@ -0,0 +1,67 @@
+const math   = require('../helpers/math');
+const Player = require('../models/player');
+const Game   = require('../models/game');
+
+const StateManager = function() {
+
+    class StateManager {
+        constructor(socket = {}) {
+            this.socket = socket;
+            this.players = {};
+            this.games = {};
+        }
+
+        findOpponent(playerX) {
+            if (!(playerX instanceof Player)) {
+        		throw 'Invalid argument playerX';
+        	}
+
+        	for (let playerId in this.players) {
+        		if (playerId === playerX.id || this.players[playerId].status !== Player.Status.AWAITING_OPPONENT) {
+        			continue;
+        		}
+
+        		const playerO = this.players[playerId];
+        		const firstTurn = (math.random() % 2 === 0) ? Game.Symbol.X : Game.Symbol.O;
+        		const game = Game.create(playerX, playerO);
+
+        		this.games[game.id] = game;
+
+        		playerX.setStatus(Player.Status.PLAYING);
+        		playerO.setStatus(Player.Status.PLAYING);
+
+        		playerX.socket.emit('game.start', {
+        			gameId: game.id,
+        			turn: firstTurn,
+        			me: {
+        				symbol: Game.Symbol.X
+        			},
+        			opponent: {
+        				symbol: Game.Symbol.O,
+        				id: game.playerO.id,
+        				name: game.playerO.name
+        			}
+        		});
+
+        		playerO.socket.emit('game.start', {
+        			gameId: game.id,
+        			turn: firstTurn,
+        			me: {
+        				symbol: Game.Symbol.O
+        			},
+        			opponent: {
+        				symbol: Game.Symbol.X,
+        				id: playerX.id,
+        				name: playerX.name
+        			}
+        		});
+
+        		break;
+        	}
+        }
+    }
+
+    return new StateManager();
+}
+
+module.exports = StateManager;
