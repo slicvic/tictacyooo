89f1b7c7a8a38614089fd281895949c58865e350
victor <dot@mymngmt.com>
2017-04-13 20:47:17 -0400

    Refactor socket logic into socket manager service

--- server.js
+++ server.js
@@ -9,48 +9,36 @@ const Player     = require('./models/player');
 const Game       = require('./models/game');
 const players    = {};
 const games      = {};
+const socketManager = require('./services/socket-manager')(io);
 
 httpServer.listen(port);
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connect', function(socket) {
-	socket.on('player.enter', function(data) {
-		if (typeof data === 'object' && typeof data.name === 'string') {
-			try {
-				const player = Player.create(data.name, socket);
-				players[player.id] = player;
-				socket.emit('player.enter', {
-					success: true,
-					playerId: player.id
-				});
-			} catch (e) {
-				socket.emit('player.enter', {
-					success: false,
-					message: "Yo this trippin' man, try again!"
-				});
-			}
-		} else {
-			socket.emit('player.enter', {
-				success: false,
-				message: "Yo that name ain't fly!"
-			});
-		}
-	});
+socketManager.onConnect(function(socket) {
 
-	socket.on('player.awaitingOpponent', function(data) {
-		if (typeof data === 'object' && typeof data.playerId === 'string' && players[data.playerId] instanceof Player) {
-			players[data.playerId].status = Player.Status.AWAITING_OPPONENT;
-			socket.emit('player.awaitingOpponent', {
-				success: true
-			});
-			findOpponent(players[data.playerId]);
-		} else {
-			socket.emit('player.awaitingOpponent', {
-				success: false,
-				message: "Yo this trippin' man, try again!"
-			});
-		}
-	});
+    this.onPlayerEnter((data) => {
+        if (typeof data === 'object' && typeof data.name === 'string') {
+            try {
+                const player = Player.create(data.name, socket);
+                players[player.id] = player;
+                this.emitPlayerEnter(true, player.id);
+            } catch (e) {
+                this.emitPlayerEnter(false, null, 'Whoops! We ran into a problem, no biggie, just try again!');
+            }
+        } else {
+            this.emitPlayerEnter(false, null, `Whoops! We didn't get your name, make sure it's at least ${Player.MINIMUM_NAME_LENGTH} characters!`);
+        }
+    });
+
+    this.onPlayerAwaitingOpponent((data) => {
+        if (typeof data === 'object' && typeof data.playerId === 'string' && players[data.playerId] instanceof Player) {
+            players[data.playerId].status = Player.Status.AWAITING_OPPONENT;
+            this.emitPlayerAwaitingOpponent(true);
+            findOpponent(players[data.playerId]);
+        } else {
+            this.emitPlayerAwaitingOpponent(false, 'Whoops! We ran into a problem, no biggie, just try again!');
+        }
+    });
 });
 
 function findOpponent(playerX) {
---
+++ services/socket-manager.js
@@ -0,0 +1,43 @@
+const SocketManager = function(io = {}) {
+
+    class SocketManager {
+        constructor(io = {}) {
+            this.io = io;
+            this.socket = null;
+        }
+
+        onConnect(callback) {
+            this.io.on('connect', (socket) => {
+                this.socket = socket;
+                callback.call(this, socket);
+            });
+        }
+
+        onPlayerEnter(callback) {
+            this.socket.on('player.enter', callback.bind(this));
+        }
+
+        onPlayerAwaitingOpponent(callback) {
+            this.socket.on('player.awaitingOpponent', callback.bind(this));
+        }
+
+        emitPlayerEnter(success, playerId, message = '') {
+            this.socket.emit('player.enter', {
+                success,
+                playerId,
+                message
+            });
+        }
+
+        emitPlayerAwaitingOpponent(success, message = '') {
+            this.socket.emit('player.awaitingOpponent', {
+                success,
+                message
+            });
+        }
+    }
+
+    return new SocketManager(io);
+}
+
+module.exports = SocketManager;
