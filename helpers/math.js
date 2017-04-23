ab2f163cd717d69ebb877814d7071d90ed0c160a
victor <dot@mymngmt.com>
2017-04-23 12:11:56 -0400

    Refactor math helper

--- helpers/math.js
+++ helpers/math.js
@@ -1,5 +1,16 @@
-function random(min = 1, max = 15) {
-	return Math.floor(Math.random() * max) + min;
+/**
+ * A math utility.
+ */
+class Math {
+	/**
+	 * Generate a random number between min and max.
+	 * @param  {Number} [min=1]
+	 * @param  {Number} [max=15]
+	 * @return {Number}
+	 */
+	random(min = 1, max = 15) {
+		return Math.floor(Math.random() * max) + min;
+	}
 }
 
-exports.random = random;
+exports.math = new Math();
