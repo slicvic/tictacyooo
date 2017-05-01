8ec1bb54e8a33cccb5ae749c4c0da5480a466f5b
victor <dot@mymngmt.com>
2017-05-01 05:51:15 -0400

    Hook up game states

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,26 @@
 var app = function (Vue) {
     const SERVER_URL = 'http://192.167.10.10:3001';
+    const STATE_JOIN = 'join';
+    const STATE_AWAITING_OPPONENT = 'awaitingOpponent';
+    const STATE_PLAYING = 'playing';
 
     return new Vue({
         el: '#app',
         data: {
+            state: STATE_JOIN
+        },
+        created: function() {
+        },
+        methods: {
+            isStateJoin() {
+                return this.state === STATE_JOIN;
+            },
+            isStateAwaitingOpponent() {
+                return this.state === STATE_AWAITING_OPPONENT;
+            },
+            isStatePlaying() {
+                return this.state === STATE_PLAYING;
+            }
         }
     });
 }(Vue);
